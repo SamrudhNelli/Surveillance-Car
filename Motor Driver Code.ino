45e4bf17250c4ebe7a15e4699c43669c65ad5ddd
SamrudhNelli <74651507+SamrudhNelli@users.noreply.github.com>
2023-11-14 11:02:15 +0530

    Update Motor Driver Code.ino

--- Motor Driver Code.ino
+++ Motor Driver Code.ino
@@ -1,124 +1,167 @@
+// DIY PROJECT // Team 18 // IIT Kharagpur //
+// Credits to : Samrudh Nelli //
+
+// Using ESP32 DEVKIT V1 //
 #define CUSTOM_SETTINGS
 #define INCLUDE_GAMEPAD_MODULE
-#include <Dabble.h>
-
-#define motor1_pin1 = 12;
-#define motor1_pin2 = 10;
-#define motor1_em1 = 11;
-#define motor2_pin1 = 4;
-#define motor2_pin2 = 2;
-#define motor2_em1 = 3;
-
-
+#include <DabbleESP32.h>
+int motor1ena = 12;
+int motor1in2 = 27;
+int motor1in1 = 26;
+int motor2enb = 32;
+int motor2in4 = 25;
+int motor2in3 = 33;
+int turn_count =0;
 
 void setup() {
-  
- Serial.begin(9600);
- Dabble.begin(38400);
-
-    pinMode(motor1_pin1, OUTPUT);
-    pinMode(motor1_pin2, OUTPUT);
-    pinMode(motor1_em1, OUTPUT);
-    pinMode(motor2_pin1, OUTPUT);
-    pinMode(motor2_pin2, OUTPUT);
-    pinMode(motor2_em1, OUTPUT);
-
+Serial.begin(9600);
+Dabble.begin("DIY TEAM 18");
+pinMode(motor1ena,OUTPUT);
+pinMode(motor1in1,OUTPUT);
+pinMode(motor1in2,OUTPUT);
+pinMode(motor2enb,OUTPUT);
+pinMode(motor2in3,OUTPUT);
+pinMode(motor2in4,OUTPUT);
 }
 
-void loop() 
-     {
-        // put your main code here, to run repeatedly:
-        Dabble.processInput();
-        if (GamePad.isUpPressed())
-          {
-            Serial.print("UP");
-            forward();
-          }
+void stop(){
+analogWrite(motor1ena,0);
+analogWrite(motor2enb,0);
+digitalWrite(motor1in1,LOW);
+digitalWrite(motor1in2,LOW);
+digitalWrite(motor2in3,LOW);
+digitalWrite(motor2in4,LOW);
 
-        else if (GamePad.isDownPressed())
-          {
-             Serial.print("DOWN");
-             backward();
-          }
+}
+void move_forward(int a,int b){
+  
+analogWrite(motor1ena,b);
+analogWrite(motor2enb,a);
+digitalWrite(motor1in1,HIGH);
+digitalWrite(motor1in2,LOW);
+digitalWrite(motor2in3,HIGH);
+digitalWrite(motor2in4,LOW);
+}
 
-       else  if (GamePad.isLeftPressed())
-           {
-              Serial.print("Left");
-              left();
-           }
+void move_forward(){
+  
+digitalWrite(motor1in1,HIGH);
+digitalWrite(motor1in2,LOW);
+digitalWrite(motor2in3,HIGH);
+digitalWrite(motor2in4,LOW);
+}
 
-       else  if (GamePad.isRightPressed())
-           {
-               Serial.print("Right");
-               right();
-           }
+void rotate(int a,int b){
+  
+analogWrite(motor1ena,b);
+analogWrite(motor2enb,a);
+digitalWrite(motor1in1,HIGH);
+digitalWrite(motor1in2,LOW);
+digitalWrite(motor2in3,HIGH);
+digitalWrite(motor2in4,LOW);
+}
 
-           else 
-           {
-            Serial.println("strop");
-            Stop();
-           }
+void rotate2(int a,int b){
+  
+analogWrite(motor1ena,b);
+analogWrite(motor2enb,a);
+digitalWrite(motor1in1,LOW);
+digitalWrite(motor1in2,HIGH);
+digitalWrite(motor2in3,LOW);
+digitalWrite(motor2in4,HIGH);
+}
+
+void move_backward(int a,int b){
+  
+analogWrite(motor1ena,b);
+analogWrite(motor2enb,a);
+digitalWrite(motor1in1,LOW);
+digitalWrite(motor1in2,HIGH);
+digitalWrite(motor2in3,LOW);
+digitalWrite(motor2in4,HIGH);
+}
+
+void move_backward(){
+  
+digitalWrite(motor1in1,LOW);
+digitalWrite(motor1in2,HIGH);
+digitalWrite(motor2in3,LOW);
+digitalWrite(motor2in4,HIGH);
+}
 
 
-     }
 
-// forward
-     void forward()
-     {
-        digitalWrite(motor1_pin1, HIGH);
-        digitalWrite(motor1_pin2, LOW); 
-        digitalWrite(motor2_pin1, LOW);
-        digitalWrite(motor2_pin2, HIGH);
-        digitalWrite(motor1_em1, HIGH);
-        digitalWrite(motor2_em1, HIGH);
+void loop(){
+Dabble.processInput(); 
+int a = GamePad.getAngle();
+if (GamePad.isUpPressed())
+  {
+    Serial.print("UP");
+    move_forward();
+  }
 
-     }   
-         
-        
-         // left
-     void left()
-     {
-        digitalWrite(motor1_pin1, HIGH);
-        digitalWrite(motor1_pin2, LOW); 
-        digitalWrite(motor2_pin1, HIGH);
-        digitalWrite(motor2_pin2, LOW);
-        digitalWrite(motor1_em1, HIGH);
-        digitalWrite(motor2_em1, LOW);
-         
-     }  
-        
-    // right
-     void right()
-     {
-        digitalWrite(motor1_pin1, LOW); 
-        digitalWrite(motor1_pin2, HIGH); 
-        digitalWrite(motor2_pin1, LOW);
-        digitalWrite(motor2_pin2, HIGH);
-        digitalWrite(motor1_em1, LOW);
-        digitalWrite(motor2_em1, HIGH);
-        
-     }  
-       
-    
-    // backward
-    void backward()
-    {
-        digitalWrite(motor1_pin1, LOW); 
-        digitalWrite(motor1_pin2, HIGH);
-        digitalWrite(motor2_pin1, HIGH);
-        digitalWrite(motor2_pin2, LOW);
-        digitalWrite(motor1_em1, HIGH);
-        digitalWrite(motor2_em1, HIGH);
-        
-    }   
-    
-     // Stop
-     void Stop()
-     {
-        digitalWrite(motor1_pin1, LOW); 
-        digitalWrite(motor1_pin2, LOW); 
-        digitalWrite(motor2_pin1, LOW);
-        digitalWrite(motor2_pin2, LOW);
-        digitalWrite(motor1_em1, LOW);
-        digitalWrite(motor2_em1, LOW); 
-     } 
+  if (GamePad.isDownPressed())
+  {
+    Serial.print("DOWN");
+    move_backward();
+  }
+
+  Serial.print("Angle: ");
+  Serial.print(a);
+  Serial.print('\t');
+  int b = GamePad.getRadius();
+  Serial.print("Radius: ");
+  Serial.print(b);
+  Serial.print('\t');
+  float c = GamePad.getXaxisData();
+  Serial.print("x_axis: ");
+  Serial.print(c);
+  Serial.print('\t');
+  float d = GamePad.getYaxisData();
+  Serial.print("y_axis: ");
+  Serial.println(d);
+  Serial.println();
+  //fdgdng
+
+    float r,l;
+    if (c>0){
+      r=c;
+      l=0;
+    }
+    else{
+      r=0;
+      l=-1*c;
+    }
+    float main_vel = (36.42 * b);
+    int right_front = floor(main_vel- 35 * r);
+    int left_front  = floor(main_vel- 35 * l);
+  if (a>60 && a<120){
+    move_forward(main_vel,main_vel);
+  }
+  else if(a>240 && a<300){
+    move_backward(main_vel,main_vel);
+  }
+  else if ((a>=0 && a<=210)||(a>300 && a<=360)){
+     move_forward(right_front,left_front);
+  }
+  else{
+     move_backward(right_front,left_front);
+  }
+if(GamePad.isCirclePressed()){
+   move_forward(0,150);
+  }
+  else if(GamePad.isSquarePressed()){
+   move_forward(150,0);
+  }
+  
+if(GamePad.isCrossPressed()){
+   rotate(150,150);
+   turn_count ++;
+  }
+  
+  else if (GamePad.isTrianglePressed()){
+   
+      rotate2(150,150);
+    }
+  
+}
